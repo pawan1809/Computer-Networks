9346100b338a59b0d156bcba8e61006802ae295e
PAWAN KUMAR <pawankumar.iiitdmj@gmail.com>
2024-07-01 23:09:02 +0530

    Add files via upload

--- Lab Work/Socket Programming/Multiplication Table/server.java
+++ Lab Work/Socket Programming/Multiplication Table/server.java
@@ -1,1 +1,59 @@
+/*
+Code For: Multiplication Table
+Author	: Pawan Kumar
+Email	: pawankumar.iiitdmj@gmail.com
+Youtube : https://www.youtube.com/@dr_pawan
+Github  : https://github.com/pawan1809
+*/
 
+
+
+import java.net.*;
+class server
+{
+
+    public static String fn(String n){
+
+        System.out.println(n.length());
+
+        int a=Integer.parseInt(n);
+
+        int ans=0;
+        while(a>0){
+            ans+=a%10;
+            a/=10;
+        }
+
+        n=Integer.toString(ans);
+        return n;
+    }
+    public static void main(String args[]) throws Exception
+    {
+        DatagramSocket serverSocket = new DatagramSocket(7009);
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
+
+
+        DatagramPacket receivePacket =new DatagramPacket(receiveData, receiveData.length);
+
+        serverSocket.receive(receivePacket);
+
+        String sentence = new String(receivePacket.getData(),0,receivePacket.getLength());
+
+        InetAddress IPAddress = receivePacket.getAddress();
+
+        int port = receivePacket.getPort();
+
+        String capitalizedSentence = fn(sentence);
+
+        sendData = capitalizedSentence.getBytes();
+
+        DatagramPacket sendPacket =new DatagramPacket(sendData, sendData.length, IPAddress,port);
+
+        System.out.println("Sending Sum to Client");
+        serverSocket.send(sendPacket);
+
+        serverSocket.close();
+    }
+
+}
